48f72b26ed1179533a172949b01310fca41dde63
Renato Montanari <renatomontanari@ymail.com>
2024-03-25 00:20:24 -0300

    create migrations blog

---
+++ database/migrations/2024_03_25_022733_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+
+            $table->string('thumb')->nullable();
+            $table->string('thumb_legenda')->nullable();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content')->nullable();
+            $table->bigInteger('views')->default(0);
+
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('featured')->default(false);
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
