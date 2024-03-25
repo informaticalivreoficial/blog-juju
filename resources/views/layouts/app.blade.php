88658b53a4511814c33e591453d84a96bfe1a3c9
Renato Montanari <renatomontanari@ymail.com>
2024-03-24 23:05:34 -0300

    update views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,26 +20,15 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        @include('layouts.partials.header')
+    
+        <main class="container mx-auto px-5 flex flex-grow">
+            {{$slot }}
+        </main>
+    
+        @include('layouts.partials.footer')
 
         @stack('modals')
-
         @livewireScripts
     </body>
 </html>
