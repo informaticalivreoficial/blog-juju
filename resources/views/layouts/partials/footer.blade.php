c552c272c1e6b9bb7275cf6860b361ba56d294bf
Renato Montanari <renatomontanari@ymail.com>
2024-03-29 01:04:28 -0300

    fix model user

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="flex flex-wrap items-center justify-between px-4 py-4 text-sm border-t border-gray-100 ">
 
     <div class="flex space-x-4">
-        @foreach (config('app.supported_locales') as $locale => $data)
-            <a href="{{ route('locale', $locale) }}">
-                <x-dynamic-component :component="'flag-country-' . $data['icon']" class="w-6 h-6" />
-            </a>
-        @endforeach
+        
     </div>
     <div class="flex space-x-4">
         <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.login') }} </a>
